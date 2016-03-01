114c812fe7de52bc3ce38c261397f85cf627d717
santosh44u <santosh44u@gmail.com>
2016-03-01 15:57:48 +0530

    first commit

---
+++ public/javascripts/errorCtrl.js
@@ -0,0 +1,4 @@
+var app = angular.module('myApp', []);
+app.controller('errorCtrl', function($scope) {
+    $scope.error = "Its Error Santosh";
+});
---
+++ public/javascripts/personCtrl.js
@@ -0,0 +1,7 @@
+angular.module('myApp', []).controller('personCtrl', function($scope) {
+    $scope.names = [
+        {name:'Jani',country:'Norway'},
+        {name:'Hege',country:'Sweden'},
+        {name:'Kai',country:'Denmark'}
+    ];
+});
